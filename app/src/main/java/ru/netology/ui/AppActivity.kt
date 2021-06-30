46adff4124f3cf3356163ff446792eb15321f70d
Fedor Garbuzov <71636417+FedorGarbuzov@users.noreply.github.com>
2021-06-30 07:09:22 +0300

    Custom View

---
+++ app/src/main/java/ru/netology/ui/AppActivity.kt
@@ -0,0 +1,19 @@
+package ru.netology.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.netology.R
+
+class AppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app)
+
+        findViewById<StatsView>(R.id.stats).data = listOf(
+            0.25F,
+            0.25F,
+            0.25F,
+            0.25F,
+        )
+    }
+}
---
+++ app/src/main/java/ru/netology/ui/StatsView.kt
@@ -0,0 +1,106 @@
+package ru.netology.ui
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.withStyledAttributes
+import ru.netology.R
+import ru.netology.util.AndroidUtils
+import java.lang.Integer.min
+import kotlin.random.Random
+
+@SuppressLint("NewApi")
+class StatsView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : View(context, attrs, defStyleAttr, defStyleRes) {
+    private var radius = 0F
+    private var center = PointF(0F, 0F)
+    private var circle = RectF(0F, 0F, 0F, 0F)
+
+    private var lineWith = AndroidUtils.dp(context, 5F).toFloat()
+    private var fontSize = AndroidUtils.dp(context, 40F).toFloat()
+    private var colors = emptyList<Int>()
+
+    init {
+        context.withStyledAttributes(attrs, R.styleable.StatsView) {
+            lineWith = getDimension(R.styleable.StatsView_lineWidth, lineWith)
+            fontSize = getDimension(R.styleable.StatsView_fontSize, fontSize)
+            colors = listOf(
+                getColor(
+                    R.styleable.StatsView_color1,
+                    randomColor()
+                ),
+                getColor(
+                    R.styleable.StatsView_color2,
+                    randomColor()
+                ),
+                getColor(
+                    R.styleable.StatsView_color3,
+                    randomColor()
+                ),
+                getColor(
+                    R.styleable.StatsView_color4,
+                    randomColor()
+                )
+            )
+        }
+    }
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.STROKE
+        strokeWidth = lineWith
+        strokeCap = Paint.Cap.ROUND
+        strokeJoin = Paint.Join.ROUND
+    }
+
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = fontSize
+    }
+
+    var data: List<Float> = emptyList()
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = min(w, h) / 2F - lineWith / 2
+        center = PointF(w / 2F, h / 2F)
+        circle = RectF(
+            center.x - radius, center.y - radius,
+            center.x + radius, center.y + radius
+        )
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        if (data.isEmpty()) return
+
+        var startFrom = -90F
+
+        for ((index, datum) in data.withIndex()) {
+            var angle = 360 * datum
+            paint.color = colors.getOrNull(index) ?: randomColor()
+            canvas.drawArc(circle, startFrom, angle, false, paint)
+            startFrom += angle
+        }
+
+        canvas.drawText(
+            "%.2f%%".format(data.sum() * 100),
+            center.x,
+            center.y + textPaint.textSize / 4,
+            textPaint,
+        )
+    }
+}
+
+private fun randomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
